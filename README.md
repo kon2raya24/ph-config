e6137ef13ac0c63850769720d2793d106290385f
kon2raya24 <konturaya@gmail.com>
2026-06-16 15:09:04 +0000

    feat: initial release of ph-config

---
+++ README.md
@@ -0,0 +1,13 @@
+# ph-config
+
+Shared configuration for Philippine projects.
+
+## Includes
+
+- ESLint config
+- Prettier config
+- PHPStan config
+
+## License
+
+MIT
---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
